2eaa5c936d2073fefdfb0f4af33e98bec0a1693d
0rotciv0 <v.halbitte@gmail.com>
2025-05-12 18:02:35 +0200

    Remise en route
    1er jour

--- image.py
+++ image.py
@@ -97,28 +97,4 @@ def remplacer_k_bits_upgrade(img, k:int, chaine:str):
         indice += 1
 
     afficher(new)
-# deb = time.time()
-# sans_k_bits(img,0)
-# fin = time.time()
-# print(fin - deb) # 2,8 sec -> 2,1 sec
 
-# sans_k_bits(img,7)
-
-
-#https://chatgpt.com/share/674f1e08-9dbc-8009-b563-3a8f8d771dbb
-
-"""     Boucle dans sans_k_bits
-
-            # print(pixel)
-            # pixel_bin = tuple( map(lambda x : format(x,'08b'), list(pixel)))
-            
-            # #print(tuple(pixel_bin))
-
-            # modif_pixel_bin = tuple( map(lambda s : s[:8-k] + ("0"*k), pixel_bin))
-            # #print(modif_pixel_bin)
-
-
-            # final_en_dec = tuple(map(lambda x : int(x,2), modif_pixel_bin))
-            # #print(final_en_dec)*
-
-"""
